22d62b7694f633571020dacf7712b28770cefcfd
“Alicia <alivingston@blend.com>
2021-05-03 00:42:57 -0700

    Added styled components and header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
 function Header() {
   return (
-    <div>
-      Hello Disney     
-    </div>
+    <Container>
+      Header
+    </Container>
   )
 }
 
 export default Header
+
+const Container = styled.div`
+    height: 70px;
+    background: #090b13
+`
