0717affc6e58d3e8f38c2b09a718910dc79daf44
“Alicia <alivingston@blend.com>
2021-05-19 17:32:46 -0700

    add Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,15 @@ import styled from 'styled-components';
 function Header() {
   return (
     <Container>
-      Header
+        Header
     </Container>
   )
-}
+} 
 
 export default Header
 
+
 const Container = styled.div`
-    height: 70px;
-    background: #090b13
+  height: 70px;
+  background: #090b13;
 `
