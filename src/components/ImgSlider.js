05d64b1b46f15ae97f67916184c33bfbe7878860
“Alicia <alivingston@blend.com>
2021-05-19 23:47:43 -0700

    added slider via npm and images with sytling

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 function ImgSlider() {
+
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true
+  }
+
   return (
-    <div>
-        Image Slider 
-    </div>
+    <Carousel {...settings}>
+        <Wrap>
+            <img src="/images/slider-badging.jpg" />
+        </Wrap> 
+        <Wrap>
+            <img src="/images/slider-badag.jpg" />
+        </Wrap>
+          <Wrap>
+            <img src="/images/slider-badging.jpg" />
+        </Wrap>  
+    </Carousel>
   )
 }
 
 export default ImgSlider
+
+const Carousel = styled(Slider)`
+    margin-toop: 20px;
+
+    .slick-list {
+        overflow: visible;
+    }    
+`
+
+const Wrap = styled.div`
+    img {
+      border: 4px solid transparent;
+      border-radius: 4px;
+      width: 100%;
+      height: 100%;
+      box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+      rgb(0 0 0 / 73%) 0px 16px 10px -10px
+    }
+`
